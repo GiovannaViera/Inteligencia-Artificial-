be2b4ca080ea4051a737779bae5b395f73fd4c50
Gi Nack <104856661+GiovannaViera@users.noreply.github.com>
2024-07-26 13:54:54 +0000

    .

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-perguntas");
+const caixaPrincipal = document.querySelector(".caixa-alternativas");
+const caixaPrincipal = document.querySelector(".caixa-resultados");
+const caixaPrincipal = document.querySelector(".texto-resultados");
 
 const perguntas = [
  pergunta1 {
