28b6f190cb66fba0f246171d7b0c278d0dc440e9
Gi Nack <104856661+GiovannaViera@users.noreply.github.com>
2024-07-26 13:08:10 +0000

    .

--- main.js
+++ main.js
@@ -2,11 +2,35 @@ const perguntas = [
  pergunta1 {
     
 enunciado:"Você gosta de bolo?",
-alternativas:['sim','não']
+alternativas:[
+{
+texto:"Alternativa A",
+afirmacao:"Você é uma pessoa normal"
+},
+
+{
+texto:"Alternativa B",
+afirmacao:"Você é chato pra caramba"
+}]
 
 },
 
 pergunta2{
+    
+    enunciado:"Você dorme?",
+    alternativas:[
+    {
+    texto:"Sim",
+    afirmacao:"Você é uma pessoa normal"
+    },
+        
+    {
+    texto:"Não",
+    afirmacao:"Você é uma anomalia do universo"
+    }]
+        
+        
+    
 
 }
 ]
